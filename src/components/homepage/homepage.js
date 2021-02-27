7937c6f6e1a0f4492d84fdd8d4d17caeb55b6325
Michael <memiller219@gmail.com>
2021-02-26 16:49:57 -0800

    initial commit with some styling and homepage

---
+++ src/components/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const Homepage = (props) => {
+  return (
+    <div className="homepage">
+      <div className="banner">Welcome Air Tech Heating & air </div>
+    </div>
+  );
+};
+
+export default Homepage;
